7f3b8e1c100daa9392df8b16de7c151255358265
David Lane <me@dvdln.com>
2016-09-08 15:46:32 -0400

    1.0.0-beta.7

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,5 @@
 'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
 var _parsers = require('./parsers');
 
 var parsers = _interopRequireWildcard(_parsers);
@@ -12,8 +8,7 @@ var _xform = require('./xform');
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
-exports.default = Object.assign(_xform.createTransformer, parsers, {
-  createTransformer: _xform.createTransformer,
+module.exports = Object.assign(_xform.createTransformer, parsers, {
   transform: _xform.transform
 });
 //# sourceMappingURL=index.js.map
